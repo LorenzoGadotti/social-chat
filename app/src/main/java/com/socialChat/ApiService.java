1a195416be3e526662790decbd7413888180a2c6
LorenzoGadotti <106266473+LorenzoGadotti@users.noreply.github.com>
2025-06-30 16:00:09 -0300

    Update ApiService.java
    Usando Gemini 2.0 ao invés do 1.5

--- app/src/main/java/com/socialChat/ApiService.java
+++ app/src/main/java/com/socialChat/ApiService.java
@@ -17,7 +17,7 @@ import java.nio.charset.StandardCharsets;
 public class ApiService {
     private static final String API_KEY = "";
     private static final String GEMINI_URL =
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key="
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key="
                     + API_KEY;
     private static final int CONNECT_TIMEOUT = 10000;
     private static final int READ_TIMEOUT = 30000;
